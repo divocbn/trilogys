096eaebb486d968e8a3bf1be098bb93b8ee887af
divo <divocbn@gmail.com>
2024-09-13 01:35:54 +0200

    add: landing

--- apps/web/src/app/(landing)/page.tsx
+++ apps/web/src/app/(landing)/page.tsx
@@ -1,14 +1,40 @@
 import { prisma } from "@portfolio/db";
+import Image from "next/image";
 
 export default function HomePage() {
   return (
-    <main className="container h-screen py-16">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Turborepo boilerplate
+    <main className="container min-h-screen pt-40">
+      <div className="flex flex-col items-center justify-center gap-4 py-12">
+        <h1 className="text-5xl font-bold tracking-tighter sm:text-[5rem]">
+          Lassen Sie uns einfach Taten sprechen.
         </h1>
+      </div>
 
-        <h2 className="text-2xl font-semibold tracking-tight">By @divocbn</h2>
+      <div className="mt-16 w-full bg-black/40 rounded-lg relative border border-white/70">
+        <Image
+          src="/raplarue.png"
+          width={2048}
+          height={0}
+          alt="Rap La Rue Project Image"
+          className="rounded-md invert "
+        />
+        <div className="absolute bottom-0 h-28 backdrop-blur-sm bg-black/70 w-full rounded-b-lg items-center flex">
+          <div className="container text-white/80 items-center">
+            <div className="flex flex-row justify-between">
+              <div className="flex flex-col">
+
+                <p className="text-xl font-medium">Projekt: Rap La Rue</p>
+                <p className="text-xs">
+                  In diesem Projekt ging es primär darum, eine Plattform bereitzustellen auf welcher Nutzer Bewerbungen in Form eines Videos hochladen können. <br />
+                  In der Administrationsoberfläche hat unser Entwicklerteam eine Plattform geboten, in welcher das Team von Rap La Rue die Bewerbungen evaluieren, sortieren & verwalten konnte.</p>
+              </div>
+
+              <div className="flex flex-col">
+
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -6,9 +6,12 @@ import { cn } from "@portfolio/ui";
 import { ThemeProvider, ThemeToggle } from "@portfolio/ui/theme";
 import { Toaster } from "@portfolio/ui/toast";
 
+import { Inter } from 'next/font/google'
+
 import "~/app/globals.css";
 
 import { env } from "~/env";
+import Balancer from "react-wrap-balancer";
 
 export const metadata: Metadata = {
   metadataBase: new URL(
@@ -16,7 +19,7 @@ export const metadata: Metadata = {
       ? "https://turbo.t3.gg"
       : "http://localhost:3000",
   ),
-  title: "Create T3 Turbo",
+  title: "Trilogys Studios",
   description: "Simple monorepo with shared backend for web & mobile apps",
   openGraph: {
     title: "Create T3 Turbo",
@@ -38,19 +41,41 @@ export const viewport: Viewport = {
   ],
 };
 
+const inter = Inter({ subsets: ['latin'] })
+
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body
         className={cn(
-          "min-h-screen bg-background font-sans text-foreground antialiased",
-          GeistSans.variable,
-          GeistMono.variable,
+          "min-h-screen bg-background text-foreground antialiased",
+          inter.className,
         )}
       >
+        <div className="h-16 bg-[#161617]/[92%] backdrop-blur-lg backdrop-saturate-150 fixed top-0 w-full flex items-center justify-center z-50">
+          <div className="max-w-5xl w-full tracking-[-0.01em] text-white/[92%] text-xs">
+            <p className="text-center">
+              <Balancer>
+                Trilogys Studios spezialisiert sich auf die Entwicklung hochperformanter, skalierbarer Anwendungen und Websites, die modernste Technologien, aktuelle Trends sowie exzellentes UI- und UX-Design vereinen.
+              </Balancer>
+            </p>
+          </div>
+        </div>
+
+        <div className="h-11 bg-[#161617]/[70%] backdrop-blur-lg backdrop-saturate-150 fixed top-16 w-full flex items-center justify-center z-50">
+          <div className="container w-full flex flex-row gap-10 tracking-[-0.01em] text-white/[85%] text-xs">
+            <p className="max-w-md">Informationen</p>
+            <p className="max-w-md">Dienstleistungen</p>
+            <p className="max-w-md">Referenzen</p>
+            <p className="max-w-md">Team</p>
+            <p className="max-w-md">Kontakt</p>
+            <p className="max-w-md">Impressum</p>
+          </div>
+        </div>
+
         {props.children}
 
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
           <div className="absolute bottom-4 right-4">
             <ThemeToggle />
           </div>
@@ -60,4 +85,3 @@ export default function RootLayout(props: { children: React.ReactNode }) {
     </html>
   );
 }
- 
