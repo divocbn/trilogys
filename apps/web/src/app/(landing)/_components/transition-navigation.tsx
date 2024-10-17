b09e0e5f3dbf1466e2c8b348c9366b80ed40b7a4
divo <divocbn@gmail.com>
2024-10-17 13:18:00 +0200

    fix: ios touch events wont be fired

--- apps/web/src/app/(landing)/_components/transition-navigation.tsx
+++ apps/web/src/app/(landing)/_components/transition-navigation.tsx
@@ -6,10 +6,22 @@ import { useTransitionRouter } from "next-view-transitions";
 
 export default function TransitionNavigation({ children, href }: React.PropsWithChildren<{ href: string }>) {
   const router = useTransitionRouter();
+  
+  const onRouterPush = () => {
+    router.push(href);
+  }
+
+  const onTouchStart = (event: React.TouchEvent<HTMLDivElement>) => {
+    event.preventDefault();
+
+    onRouterPush();
+  }
+
   return (
     <div
       className="max-w-md hover:scale-95 transition-transform cursor-pointer"
-      onClick={() => router.push(href)}
+      onClick={onRouterPush}
+      onTouchStart={onTouchStart}
     >
       {children}
     </div>
