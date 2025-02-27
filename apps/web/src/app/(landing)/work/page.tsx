d8e43e521af7ff68ea31468718f1f81b0ee5493b
Divo <divocbn@gmail.com>
2025-02-27 19:00:25 +0100

    feat: remove some unused components

--- apps/web/src/app/(landing)/work/page.tsx
+++ apps/web/src/app/(landing)/work/page.tsx
@@ -88,11 +88,6 @@ export default function WorkPage() {
       <div className="sm:mx-32 md:mx-16 mx-8 sm:bg-black/10 h-0.5 sm:mb-20" />
 
       <KfcWorkPage />
-
-      <Seperator />
-
-      <KindeWorkPage />
-
     </main>
   );
 }
